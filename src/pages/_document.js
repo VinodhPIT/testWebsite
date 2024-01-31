797eacd2ac7dbbf070bc86c09824da8fe5b01985
Binu Mohan <binu.mm@pitsolutions.com>
2024-01-31 17:42:05 +0530

    Bugfix 10265 customer analytics UI fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
         </body>
       </Html>
     );
