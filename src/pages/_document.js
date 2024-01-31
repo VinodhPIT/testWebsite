eea64919441f80522d18203286fc2a9041161b30
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-01-31 18:28:43 +0530

    No-sync  script issue fixed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,24 @@
-// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
- 
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossOrigin="anonymous"
+            defer
+          ></script>
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
         </body>
       </Html>
     );
   }
 }
- 
+
 export default MyDocument;
