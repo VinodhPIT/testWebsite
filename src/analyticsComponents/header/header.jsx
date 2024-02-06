a67a338ee8e6d259ed607875626fb1780e103c7b
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-02-06 20:43:31 +0530

    Table track artist conversion with filter

--- src/analyticsComponents/header/header.jsx
+++ src/analyticsComponents/header/header.jsx
@@ -1,130 +1,148 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Header({ data }) {
   const handleSignOut = async () => {
     await signOut({ redirect: false });
   };
+  const router = useRouter();
+
+  const navigationItems = [
+    { href: "/analytics/customer", label: "Customer" },
+    { href: "/analytics/artist", label: "Artists" },
+  ];
 
  
+
+
   return (
-    <> 
-    <header className="header_wrapper db_header_wrap">
-      <div className="container-fluid pr_40 m_pr_12">
-        <div className="row">
-          <div className="col-md-12">
-            <nav className="header_dashboard">
-              <div className="header_logo_nav">
-                <div className="header_logo">
-                  <div  className="navbar_brand">
-                    <Image
-                      src="/Inckd-logo-footer-black.svg"
-                      alt="Logo"
-                      width="105"
-                      height="31"
-                      priority
-                    />
+    <>
+      <header className="header_wrapper db_header_wrap">
+        <div className="container-fluid pr_40 m_pr_12">
+          <div className="row">
+            <div className="col-md-12">
+              <nav className="header_dashboard">
+                <div className="header_logo_nav">
+                  <div className="header_logo">
+                    <div className="navbar_brand">
+                      <Image
+                        src="/Inckd-logo-footer-black.svg"
+                        alt="Logo"
+                        width="105"
+                        height="31"
+                        priority
+                      />
+                    </div>
+                    <div className="header_db_title_logo">
+                      <h6>Analytics</h6>
+                    </div>
                   </div>
-                  <div className="header_db_title_logo">
-                    <h6>Analytics</h6>
-                  </div>
-                </div>
-                {/* <div className="header_db_title">
+                  {/* <div className="header_db_title">
                   <h4>Customer</h4>
                 </div> */}
-              </div>
-              <div className="nav_block db_navigation">
-                <ul className="nav main_nav">
-                  <li className="nav_item active">
-                    <Link href="#">Customer</Link>
-                  </li>
-                  <li className="nav_item">
-                    <Link href="#">Menu 2</Link>
-                  </li>
-                  <li className="nav_item">
-                    <Link href="#">Menu 3</Link>
-                  </li>
-                  <li className="nav_item">
-                    <Link href="#">Menu 4</Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="header_right">
-                <div className="db_user_profile">
-                  <span style={{ textTransform: "capitalize" }}>
-                    Hello, {data}{" "}
-                  </span>
-                  <Image
-                    src="/db_user_1.png"
-                    alt="user"
-                    width="40"
-                    height="40"
-                    priority
-                    className="hidden"
-                  />
+                </div>
+                <div className="nav_block db_navigation">
+                  <ul className="nav main_nav">
+                    {navigationItems.map((item, index) => (
+                      <li
+                        key={index}
+                        className={`nav_item ${
+                          router.pathname === item.href ? "active" : ""
+                        }`}
+                      >
+                        <Link href={item.href}>{item.label}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="header_right">
+                  <div className="db_user_profile">
+                    <span style={{ textTransform: "capitalize" }}>
+                      Hello, {data}{" "}
+                    </span>
+                    <Image
+                      src="/db_user_1.png"
+                      alt="user"
+                      width="40"
+                      height="40"
+                      priority
+                      className="hidden"
+                    />
+                    <button
+                      className="btn_secondary ml_10"
+                      onClick={handleSignOut}
+                    >
+                      {" "}
+                      Log out
+                    </button>
+                  </div>
                   <button
-                    className="btn_secondary ml_10"                    
-                    onClick={handleSignOut}
+                    class="nav_btn_toggle"
+                    type="button"
+                    data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasRight"
+                    aria-controls="offcanvasRight"
+                   
+                    
                   >
-                    {" "}
-                    Log out
-                  </button>
-                </div>  
-                <button class="nav_btn_toggle" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-                  <Image                    
-                    src="/blackHamburger.svg"
-                    alt="hamburger"
-                    width={32}
-                    height={32}
-                    priority
-                  />
-                </button>              
-              </div>
-
-
-              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">  
-                <div class="offcanvas-header">
-                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
-                    <Image                      
-                      src="/close.png"
-                      width={50}
-                      height={50}
-                      alt="close"
+                    <Image
+                      src="/blackHamburger.svg"
+                      alt="hamburger"
+                      width={32}
+                      height={32}
                       priority
                     />
-                  </button>  
+                  </button>
                 </div>
-                <div class="offcanvas-body">
-                  <div className="nav_block">              
-                    <ul className="nav main_nav">
-                      <li className="nav_item active">
-                        <Link href="#">Customer</Link>
-                      </li>
-                      <li className="nav_item">
-                        <Link href="#">Menu 2</Link>
-                      </li>
-                      <li className="nav_item">
-                        <Link href="#">Menu 3</Link>
-                      </li>
-                      <li className="nav_item">
-                        <Link href="#">Menu 4</Link>
-                      </li>
-                    </ul>
+
+                <div
+                  class="offcanvas offcanvas-end"
+                  tabindex="-1"
+                  id="offcanvasRight"
+                  aria-labelledby="offcanvasRightLabel"
+                >
+                  <div class="offcanvas-header">
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="offcanvas"
+                      aria-label="Close"
+                    >
+                      <Image
+                        src="/close.png"
+                        width={50}
+                        height={50}
+                        alt="close"
+                        priority
+                      />
+                    </button>
+                  </div>
+                  <div class="offcanvas-body">
+                    <div className="nav_block">
+                      <ul className="nav main_nav">
+                        {navigationItems.map((item, index) => (
+                          <li
+                            key={index}
+                            className={`nav_item ${
+                              router.pathname === item.href ? "active" : ""
+                            }`}
+                          >
+                            <Link href={item.href}>{item.label}</Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-
-
-
-
-            </nav>
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
     </>
   );
 }
---
+++ src/pages/analytics/artist/index.jsx
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import {
+  analyticsArtistCount,
+  analyticsArtistLeadSourceCount,
+} from "@/action/analyticsArtist";
+import Header from "@/analyticsComponents/header/header";
+import CustomerDetails from "@/analyticsComponents/customerDetails/customerDetails";
+import TotalCustomers from "@/analyticsComponents/totalCustomers/totalCustomers";
+import PieChart from "@/analyticsComponents/pieChart/chart";
+import ArtistConversion from "@/analyticsComponents/artistConversion/artistConversion";
+import { getSession } from "next-auth/react";
+import ComparisonChart from "@/analyticsComponents/comparisonPiechart/comparisonChart";
+import { artistConversion } from "@/analyticsComponents/customerConversion/keys";
+
+import ArtistsByCountryTable from "@/analyticsComponents/ArtistsByCountryTable/ArtistsByCountryTable";
+
+export default function Analytics({ data }) {
+  const router = useRouter();
+  const { status, data: sessionData } = useSession();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/analytics/login");
+    }
+  }, [status, router]);
+
+  const getValues = Object.values(data.genderCount);
+
+  const getKeys = Object.keys(data.genderCount).map((key) => {
+    switch (key) {
+      case "male_count":
+        return "Male";
+      case "female_count":
+        return "Female";
+      case "non_binary_count":
+        return "Other";
+      default:
+        return key;
+    }
+  });
+
+  const getColor = ["#1976D2", "#FF80FF", "#EAEAEA"];
+
+  const label = [
+    { id: 1, label: "Male", bgColor: "block_bg_blue_150" },
+    { id: 2, label: "Female", bgColor: "block_bg_pink_100" },
+    { id: 3, label: "Other", bgColor: "block_bg_gray_light_200" },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Artist-Analytics</title>
+      </Head>
+
+      <Header data={status === "authenticated" && sessionData.user.name} />
+
+      <section className="pt_20 pb_20 block_bg_gray_150">
+        <section className="container-fluid">
+          <div className="db_customer_detail_wrap">
+            <div className="row">
+              <div className="col-lg-8 col-md-6 col-sm-12">
+                <TotalCustomers
+                  title="Total Artists"
+                  chartData={data.chartData}
+                />
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <PieChart
+                  title="Total Artists by gender"
+                  getKeys={getKeys}
+                  getValues={getValues}
+                  getColor={getColor}
+                  label={label}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="container-fluid">
+          <div className="db_customer_detail_wrap">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <ComparisonChart
+                  totalData={data.chartData}
+                  title="Normal vs Referred artists"
+                  labe_1="Normal artists"
+                  labe_2="Referred artists"
+                />
+              </div>
+              <div className="col-lg-8 col-md-6 col-sm-12">
+                <ArtistsByCountryTable data={data.chartData} />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="container-fluid">
+          <div className="db_customer_detail_wrap">
+            <div className="row">
+              <div className="col-lg-12 col-md-12 col-sm-12">
+                <ArtistConversion
+                  title={"Artist Conversion"}
+                  token={data.sessionToken}
+                  types={artistConversion}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/analytics/login",
+        permanent: false,
+      },
+    };
+  }
+
+  try {
+    const [data, customerJoinigData] = await Promise.all([
+      analyticsArtistCount(session.user.myToken),
+      analyticsArtistLeadSourceCount(session.user.myToken),
+    ]);
+
+    return {
+      props: {
+        data: {
+          chartData: customerJoinigData ?? [],
+          genderCount: data.gender || 0,
+          sessionToken: session.user.myToken ?? "",
+        },
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+}
