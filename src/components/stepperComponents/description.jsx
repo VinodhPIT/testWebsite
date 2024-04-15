3fb6057663cf175af64a37199254f45579dfa9e5
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-04-15 21:46:34 +0530

    Added alert box if  user type description  text that exceeds length greater than 500

--- src/components/stepperComponents/description.jsx
+++ src/components/stepperComponents/description.jsx
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import { useRequestForm } from "@/store/requestManagement/requestForm"; 
 import useTranslation from "next-translate/useTranslation";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Description = () => {
@@ -9,7 +11,6 @@ const Description = () => {
   const { t } = useTranslation();
   const handleSubmit = (event) => {
     event.preventDefault();
-
     nextPage();
   };
 
@@ -26,10 +27,26 @@ const Description = () => {
                   <form onSubmit={handleSubmit}>
                     <textarea
                       value={message}
-                      onChange={(event) => setDescription(event.target.value)}
+                      onChange={(event) => {
+                        if (event.target.value.length <= 500) {
+                          setDescription(event.target.value);
+                        }
+                        else {
+                          alert('You can write only up to 500 characters.'); 
+                        }
+                      }}
                       placeholder={t("common:stepper.typeDescription")}
                       rows={4} // Set the number of rows for the textarea
                       cols={50} // Set the number of columns for the textarea
+                      onPaste={(event) => {
+                        const pastedText = event.clipboardData.getData('text/plain');
+                        if (message.length + pastedText.length > 500) {
+                          event.preventDefault();
+                          alert('You can only paste up to 500 characters.');
+                        
+                        }
+                      }}
+                      
                     />
                     {message && <button type="submit" className="btn_secondary btn_cutom_40 mt_15 pull_right">{t("common:next")}</button>}
                   </form>
