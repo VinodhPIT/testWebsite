33278ffc984af2c4cc78addd7590f17458799e56
Ijaz <ijaz.as@pitsolutions.com>
2024-04-19 09:14:55 +0530

    Calculate and display the average time for customers to send request

--- src/analyticsComponents/dashboard/newDashboardDetails.jsx
+++ src/analyticsComponents/dashboard/newDashboardDetails.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import "@hassanmojab/react-modern-calendar-datepicker/lib/DatePicker.css";
 import NewCountDisplayCard from "../common/newCountDisplayCard";
 import useTranslation from "next-translate/useTranslation";
+import moment from "moment";
 
 export default function NewDashboardDetails({ initialCounts, token }) {
   const { t } = useTranslation();
-  const singleRequestCount = initialCounts.filter(item => item.single_request === true);
-  const allRequestCount = initialCounts.filter(item => item.multiple_request === true);
-  const conversionRate = Math.floor((allRequestCount.length / initialCounts.length) * 100);
+  const allRequestProfiles = initialCounts.filter(item => item.multiple_request);
+  const singleRequestProfiles = initialCounts.filter(item => item.single_request);
+  const conversionRate = Math.floor((allRequestProfiles.length / initialCounts.length) * 100);
+
+  const getAverageTime=()=>{
+    const totalDurationInMinutes= allRequestProfiles.reduce((accumulator, currentValue) => {
+        const profileCreatedTimeMoment=moment(currentValue.profile_created_time);
+        const chatCreatedTimeMoment=moment(currentValue.first_chat_time);
+
+        return accumulator + (chatCreatedTimeMoment.diff(profileCreatedTimeMoment, 'minutes'))
+    },0);
+    const hours=Math.floor(totalDurationInMinutes/allRequestProfiles.length / 60)
+    const minutes=Math.floor(totalDurationInMinutes/allRequestProfiles.length % 60)
+
+    return {hours, minutes}
+  };
 
   return (
     <section className="container-fluid">
@@ -21,13 +35,13 @@ export default function NewDashboardDetails({ initialCounts, token }) {
           />
           <NewCountDisplayCard
             iconBgColor="block_bg_green_200"
-            count={singleRequestCount.length}
+            count={singleRequestProfiles.length}
             title="Single Request"
             icon="/chat-round-dots.svg"
           />
 
           <NewCountDisplayCard
-            count={allRequestCount.length}
+            count={allRequestProfiles.length}
             iconBgColor="block_bg_green_200"
             icon="/chat-round-dots.svg"
             title="All Request"
@@ -47,7 +61,7 @@ export default function NewDashboardDetails({ initialCounts, token }) {
             withRightIcon
             iconBgColor="block_bg_blue_200"
             rightIconBgColor="block_bg_green_200"
-            count={120}
+            count={`${getAverageTime().hours}h ${getAverageTime().minutes}min`}
             icon="/users.svg"
             rightIcon="/chat-round-dots.svg"
             title="Avg.time"
