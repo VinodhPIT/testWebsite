f338f476c174c572b3e624d0885d5e421d91c61a
Binu Mohan <binu.mm@pitsolutions.com>
2024-04-22 10:39:48 +0530

    feature-11310/Design-updates-for-tattoo-journal

--- src/components/tattooJournal/TattooJournal.jsx
+++ src/components/tattooJournal/TattooJournal.jsx
@@ -19,8 +19,8 @@ export default function ListingPage({ data, error }) {
   let sliderSettings = {};
 
   sliderSettings = {
-    infinite: true,
-    arrows: true,
+    infinite: false,
+    arrows: isMobileView ? false : true,
     speed: 300,
     slidesToShow: isMobileView ? 1 : 3,
     slidesToScroll: isMobileView ? 1 : 3,
@@ -56,7 +56,6 @@ export default function ListingPage({ data, error }) {
       {
         breakpoint: 900,
         settings: {
-          infinite: true,
           slidesToShow: 2,
           slidesToScroll: 3,
         },
@@ -64,7 +63,6 @@ export default function ListingPage({ data, error }) {
       {
         breakpoint: 600,
         settings: {
-          infinite: true,
           slidesToShow: 1,
           slidesToScroll: 1,
         },
@@ -73,7 +71,6 @@ export default function ListingPage({ data, error }) {
       {
         breakpoint: 400,
         settings: {
-          infinite: true,
           slidesToShow: 1,
           slidesToScroll: 1,
         },
@@ -87,16 +84,16 @@ export default function ListingPage({ data, error }) {
           <div className="justify_content_start  container w_100pc">
      
 
-            <div className="text_box_content_inner m_pr_0 pt_35 pb_40 max_w_100pc m_pt_0 m_pb_30 m_mb_25 m_mt_25">
+            <div className="text_box_content_inner m_pr_0 pt_35 pb_40 max_w_100pc m_pt_0 m_pb_20 m_mb_0 m_mt_25">
               <h2 className="color_gray_550 heading_h2 lh_40 mb_10 m_mb_0 m_text_left position_relative">
                 <span className="heading_with_arrow position_relative"> {t("common:homePage.Tattoo Journal")}</span>
               </h2>
-              <p className="custom_fs_18 color_gray_550 m_mt_15 mb_0 m_text_left fw_300">
+              <p className="custom_fs_18 custom_fs_m_14 color_gray_550 m_mt_15 mb_0 m_text_left fw_300">
               {t("common:homePage.journalContent")}
               </p>
             </div>
             <div
-              className={`${"mb_80 m_mb_30 m_mt_25"} ${
+              className={`${"mt_0 mb_80 m_mb_30 slider_nav_arrows"} ${
                 styles.listing_pageContainer
               }`}
             >
@@ -106,7 +103,7 @@ export default function ListingPage({ data, error }) {
               >
                 {data.map((el, index) => (
                   <div
-                    className={`${"listing_gridItem"} ${
+                    className={`${"listing_gridItem pl_0 pr_10"} ${
                       styles.listing_gridItem
                     }`}
                     key={index}
@@ -142,22 +139,21 @@ export default function ListingPage({ data, error }) {
                         <h6 className={styles.listing_grid_profile_title}>
                           {el.title}
                         </h6>
-                        <div className="mb_5"></div>
                         <p className={styles.listing_grid_content_disc}>
                           {el.desc}
                         </p>
-                        <div className="w_100pc d_flex justify_content_end mt_20">
+                        <div className="w_100pc d_flex justify_content_end mt_15">
                           <Link
                             href={el.url}
-                            className="btn_primary btn_img btn_custom_48"
+                            className={styles.link_with_arrow}
                           >
                             Read more
                             <Image
-                              src="/arow-white-right.svg"
-                              width={24}
-                              height={24}
+                              src="/arrowBlack.svg"
+                              width={16}
+                              height={16}
                               alt="arrow"
-                              className="ml-8 mt-2"
+                              className="mt-2"
                             />
                           </Link>
                         </div>
