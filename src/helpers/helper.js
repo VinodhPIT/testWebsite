fd09abf22912c587f30f8a0e4475593bd8e334a8
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-04-29 12:04:29 +0530

    Functional issues in the customer request form  - Set 3

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,5 +1,25 @@
 import moment from "moment";
 
+
+
+
+const getPaginatorCount = () => {
+  // Check if window is defined (only run on the client-side)
+  if (typeof window !== 'undefined') {
+    const isMobile = window.innerWidth <= 768; // Adjust the threshold as needed
+    return isMobile ? 10 : 9;
+  }
+  // Return a default value if window is not available (e.g., for server-side rendering)
+  return 10; // or any default value you prefer
+};
+
+export { getPaginatorCount };
+
+
+const pageCount = getPaginatorCount();
+
+
+
 export const prepareRequest = (parameters) => {
   const request = {
     sort: parameters.sort,
@@ -41,11 +61,11 @@ const createRequestObject = (parameters, paginatorCount) => {
 };
 
 
-const artistRequest = (parameters, paginatorCount) => {
+const artistRequest = (parameters) => {
   const request = {
     sort:null,
     page_no: parameters.page_no,
-    paginator_count: paginatorCount,
+    paginator_count: pageCount,
     search_key: parameters.search_key,
     distance:"50km",
     
@@ -77,7 +97,7 @@ const artistRequest = (parameters, paginatorCount) => {
 
 
 export const stepperParam = (parameters) => {
-  return artistRequest(parameters, 9);
+  return artistRequest(parameters);
 };
 
 export const searchParam = (parameters) => {
