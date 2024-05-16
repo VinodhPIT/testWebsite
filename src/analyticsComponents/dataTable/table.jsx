b5242b73499766b7356471d648892aa3bc3b9020
Binu Mohan <binu.mm@pitsolutions.com>
2024-05-16 11:49:10 +0530

    feature-11585/Design-integration-for-analytics-page-tab-datatable-update

---
+++ src/analyticsComponents/dataTable/table.jsx
@@ -0,0 +1,154 @@
+import React, { useMemo } from "react";
+import {
+  useTable,
+  useSortBy,
+  useFilters,
+  useGlobalFilter,
+  usePagination,
+} from "react-table";
+import style from "./table.module.css";
+import Image from "next/image";
+
+const DataTable = ({ columns, data }) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    gotoPage,
+    pageCount,
+    pageSize,
+    setPageSize,
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0, pageSize: 18 }, 
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const { pageIndex, globalFilter } = state;
+
+  const Pagination = React.memo(() => (
+    <div className={style.pagination}>
+      <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+        {"Previous"}
+      </button>
+      <span>
+        Page
+        <strong>
+          {pageIndex + 1} of {pageOptions.length}
+        </strong>{" "}
+      </span>
+      <button onClick={() => nextPage()} disabled={!canNextPage}>
+        {"Next"}
+      </button>
+    </div>
+  ));
+
+  const filterByStatus = (status) => {
+    setGlobalFilter(status);
+  };
+
+  const Button = ({ status }) => (
+    <button className="status_indicator block_bg_yellow_300 color_orange_500" onClick={() => filterByStatus(status)}>
+      {status}
+    </button>
+  );
+
+  return (
+    <div>
+      <div className={style.search}>
+        <input
+          value={globalFilter || ""}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Search..."
+        />
+        <div className="d-flex mt_20 mb_20 justify_space_between">
+          {['Scheduled', 'Completed', 'Cancelled', 'Pending', 'Rejected'].map((status) => (
+            <Button key={status} status={status} />
+          ))}
+        </div>
+      </div>
+      <table {...getTableProps()} className={style.data_table}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  key={column.id}
+                >
+                  <div className={style.wrapper}>
+                    {column.render("Header")}
+                    <span>
+                      {column.isSorted ? (
+                        column.isSortedDesc ? (
+                          <Image
+                            src="/sort_down.svg"
+                            width={17}
+                            height={16}
+                            alt="Desc"
+                          />
+                        ) : (
+                          <Image
+                            src="/sort_up.svg"
+                            width={17}
+                            height={16}
+                            alt="Asec"
+                          />
+                        )
+                      ) : (
+                        ""
+                      )}
+                    </span>
+                  </div>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        {rows.length > 0 ? (
+          <tbody {...getTableBodyProps()}>
+            {page.map((row, index) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} key={index}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()} key={cell.column.id}>
+                      {cell.render("Cell")}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        ) : (
+          <tbody>
+            <tr key="no-data">
+              <td colSpan={columns.length} className="no-data-found">
+                No data found.
+              </td>
+            </tr>
+          </tbody>
+        )}
+      </table>
+      {rows.length > 0 && <Pagination />}
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/analytics/profile/index.jsx
+++ src/pages/analytics/profile/index.jsx
@@ -12,11 +12,9 @@ import {
 import YourComponent from "@/analyticsComponents/common/keys";
 import useTranslation from "next-translate/useTranslation";
 
+import DataTable from "@/analyticsComponents/dataTable/table";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
-
-
-
 
 export default function ArtistAnalytics({ data: initialData }) {
   const { status, data } = useSession();
@@ -53,6 +51,545 @@ export default function ArtistAnalytics({ data: initialData }) {
     }
   }, [status, router]);
 
+  const columns = [    
+    {
+      Header: "Offer code",
+      accessor: "offer_code",
+    },
+    {
+      Header: "Offer Date",
+      accessor: "offer_date",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
+    },
+    {
+      Header: "Total Amount",
+      accessor: "total_amount",
+    },
+
+    {
+      Header: "Currency",
+      accessor: "currency",
+    },
+
+    {
+      Header: "Status1",
+      accessor: "status1",
+    },
+    {
+      Header: "Total Amount1",
+      accessor: "total_amount1",
+    },
+
+    {
+      Header: "Currency1",
+      accessor: "currency1",
+    },
+
+    {
+      Header: "Status11",
+      accessor: "status11",
+    },
+    {
+      Header: "Total A1modcdcdunt1",
+      accessor: "t1otal_adcdcdcmount1",
+    },
+
+    {
+      Header: "Cur1rencysxs1",
+      accessor: "cur1rencsxxsy1",
+    },
+
+    {
+      Header: "Sta11tus1",
+      accessor: "s11tatus1",
+    },
+    {
+      Header: "Total Amoun111t1",
+      accessor: "total_dcdcamount1",
+    },
+
+    {
+      Header: "Currency1wdd",
+      accessor: "currenccdcdcy1",
+    },
+
+    {
+      Header: "Total Amocdcdcun111t1",
+      accessor: "total_dcdcdcdcamount1",
+    },
+
+    {
+      Header: "Currencydcd1wdd",
+      accessor: "currencdcdccdcdcy1",
+    },
+  ];
+
+  const data1 = [
+    {
+      offer_code: null,
+      offer_date: null,  
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-21",
+      status: "cancelled",
+      total_amount: null,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-07-26",
+      status: "completed",
+      total_amount: 50.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-29",
+      status: "completed",
+      total_amount: 460.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-30",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 50.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-30",
+      psp: null,
+      status: "completed",
+      total_amount: 210.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-01",
+      psp: "klarna",
+      status: "cancelled",
+      total_amount: 15.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-07-27",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-30",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-29",
+      psp: null,
+      status: "completed",
+      total_amount: 42.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-05",
+      psp: null,
+      status: "completed",
+      total_amount: 60.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-05",
+      psp: null,
+      status: "completed",
+      total_amount: 60.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-07-31",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-14",
+      psp: null,
+      status: "cancelled",
+      total_amount: null,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-08-02",
+      psp: null,
+      status: "completed",
+      total_amount: 50.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-30",
+      psp: null,
+      status: "cancelled",
+      total_amount: 15.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-30",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 50.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-08-09",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 75.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-29",
+      psp: null,
+      status: "completed",
+      total_amount: 300.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-29",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-30",
+      psp: null,
+      status: "completed",
+      total_amount: 210.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-08-10",
+      psp: null,
+      status: "completed",
+      total_amount: 350.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-08-10",
+      psp: null,
+      status: "completed",
+      total_amount: 200.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-31",
+      psp: null,
+      status: "completed",
+      total_amount: 100.0,
+      currency: "GBP",
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-29",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-29",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-31",
+      psp: null,
+      status: "completed",
+      total_amount: 80.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-05-31",
+      psp: null,
+      status: "cancelled",
+      total_amount: 15.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-01",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 50.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: null,
+      offer_date: null,
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-05",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 130.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-01",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 60.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-16",
+      psp: null,
+      status: "scheduled",
+      total_amount: null,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-01",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 250.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-13",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-22",
+      psp: null,
+      status: null,
+      total_amount: null,
+      currency: null,
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-05",
+      psp: "klarna",
+      status: "completed",
+      total_amount: 50.0,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2024-02-12",
+      psp: null,
+      status: "pending",
+      total_amount: null,
+      currency: "CHF",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-08-30",
+      psp: null,
+      status: "rejected",
+      total_amount: null,
+      currency: "EUR",
+    },
+    {
+      offer_code: "220615456",
+      offer_date: "2023-06-22",
+      psp: null,
+      status: "rejected",
+      total_amount: null,
+      currency: "EUR",
+    },
+  ];
+
+  const datas = data1.filter((e) => e.status !== null);
+  
+
+
+
+
+
   return (
     <>
       <Head>
@@ -87,7 +624,7 @@ export default function ArtistAnalytics({ data: initialData }) {
                   </div>                  
                 </div>
                 <div class="user_profile_link">
-                <button type="button" className="btn_secondary block_bg_green_800">Public</button>
+                <button type="button" className="btn_secondary block_bg_green_800 btn_public">Public</button>
                 </div>
               </div>                          
             </div>              
@@ -100,27 +637,31 @@ export default function ArtistAnalytics({ data: initialData }) {
           <div className="row">
             <div className="col-lg-12 col-md-12">
               <Tabs className={"db_tab_block"}>
-                <TabList>
-                  <Tab>Profile</Tab>
-                  <Tab>Offers</Tab>
-                  <Tab>Chats</Tab>
-                  <Tab>Payouts</Tab>
-                  <Tab>Studios</Tab>
-                  <Tab>Customer</Tab>
-                  <Tab>Request</Tab>
-                </TabList>
+                <div className="testone">
+                  <TabList>
+                    <Tab>Profile</Tab>
+                    <Tab>Offers</Tab>
+                    <Tab>Chats</Tab>
+                    <Tab>Payouts</Tab>
+                    <Tab>Studios</Tab>
+                    <Tab>Customer</Tab>
+                    <Tab>Request</Tab>
+                  </TabList>                 
+
+
+                </div>
                 <TabPanel>
-                  <div class="profile_info_col">
-                    <div class="profile_info_head">
+                  <div class="db_main_info_col">
+                    <div class="db_main_info_head">
                       <h3 class="color_gray_550 custom_fs_26 fw_400 mb_0">My Profile</h3>
-                      <ul class="profile_status_block">
+                      <ul class="head_status_block">
                         <li>
-                          <h6 class="profile_status_label">Profile status</h6>
-                          <span class="profile_status_indicator">Public</span>
+                          <h6 class="head_status_label">Profile status</h6>
+                          <span class="status_indicator block_bg_green_100 color_green_900">Public</span>
                         </li>
                         <li>
-                          <h6 class="profile_status_label">Profile public date:</h6>
-                          <span class="profile_status_date">14/02/2024</span>
+                          <h6 class="head_status_label">Profile public date:</h6>
+                          <span class="head_status_date">14/02/2024</span>
                         </li>
                       </ul>
                     </div>
@@ -272,7 +813,7 @@ export default function ArtistAnalytics({ data: initialData }) {
                   </div>
                 </TabPanel>
                 <TabPanel>
-                  Your component  need to  come here
+                  <DataTable columns={columns} data={datas} />
                 </TabPanel>                  
                 
               </Tabs>
@@ -280,14 +821,7 @@ export default function ArtistAnalytics({ data: initialData }) {
             </div>
           </div>
         </div>
-      </section>
-
-
-
-
-
-
-      
+      </section>      
      
 
 
