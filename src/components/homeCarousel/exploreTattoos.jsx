8883e5d699d25d31fbe2732af7815280ce87dd7e
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-06-06 18:00:51 +0530

    Removed unused Component

--- src/components/homeCarousel/exploreTattoos.jsx
+++ src/components/homeCarousel/exploreTattoos.jsx
@@ -14,7 +14,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { UseSliderSetting } from "@/utils/sliderUtils";
 
-import CustomDots from '@/components/homeCarousel/cot'
+
 
 export default function ExploreTattoos({ title, content, data }) {
   const { isVisible,isSmallDevice } = useWindowResize();
