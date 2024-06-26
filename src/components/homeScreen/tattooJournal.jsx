80f9ee35a7f531ba8ea3d53a3a41e716afa48f95
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-06-26 12:41:13 +0530

    unwanted space removed

--- src/components/homeScreen/tattooJournal.jsx
+++ src/components/homeScreen/tattooJournal.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+
 import useTranslation from "next-translate/useTranslation";
 
 import useWindowResize from "@/hooks/useWindowSize";
