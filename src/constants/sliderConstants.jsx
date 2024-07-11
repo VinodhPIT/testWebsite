4b8ee78f57f64a7f5d8d2bd49839e30bcef07dab
Binu Mohan <binu.mm@pitsolutions.com>
2024-07-11 17:47:51 +0530

    bugFix-12924/Issues-related-to-the-choose-language-and-become-an-artist-pages

--- src/constants/sliderConstants.jsx
+++ src/constants/sliderConstants.jsx
@@ -3,6 +3,7 @@ export const SLIDES_TO_SHOW_DEFAULT =  4;
 export const SLIDES_TO_SHOW_ALTERNATE = 3;
 
 // Number of slides to scroll on mobile
-export const SLIDE_MOBILE_TO_SCROLL = 1.25; 
-export const SLIDE_MOBILE_TO_SCROLL_ALTERNATE = 1.2;
+export const SLIDE_MOBILE_TO_SCROLL = 1.3; 
+export const SLIDE_MOBILE_TO_SCROLL_REC = 1.47; 
+export const SLIDE_MOBILE_TO_SCROLL_ALTERNATE = 1.3;
 export const SINGLE_ITEM_SCROLL = 1;
