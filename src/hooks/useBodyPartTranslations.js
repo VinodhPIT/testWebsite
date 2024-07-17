105cd4537de46f235b5e3f7878fb720b8838ada6
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-07-17 15:36:40 +0530

    updated bodyPart translations key

--- src/hooks/useBodyPartTranslations.js
+++ src/hooks/useBodyPartTranslations.js
@@ -4,13 +4,13 @@ const useBodyPartTranslations = () => {
   const { t } = useTranslation();
 
   const tattooValues = [
-    { id: 0, title: t("common:stepper.head"), key: "Head" },
-    { id: 1, title: t("common:stepper.upperBody"), key: "Upper Body" },
-    { id: 2, title: t("common:stepper.back"), key: "Back" },
-    { id: 3, title: t("common:stepper.arm"), key: "Arm" },
-    { id: 4, title: t("common:stepper.leg"), key: "Leg" },
-    { id: 5, title: t("common:stepper.foot"), key: "Foot" },
-    { id: 6, title: t("common:stepper.other"), key: "Other" },
+    { id: 0, title: t("common:stepper.head"), key: "head" },
+    { id: 1, title: t("common:stepper.upperBody"), key: "upper_body" },
+    { id: 2, title: t("common:stepper.back"), key: "back" },
+    { id: 3, title: t("common:stepper.arm"), key: "arm" },
+    { id: 4, title: t("common:stepper.leg"), key: "leg" },
+    { id: 5, title: t("common:stepper.foot"), key: "foot" },
+    { id: 6, title: t("common:stepper.other"), key: "other" },
     { id: 7, title: t("common:stepper.dontKnowYet"), key: "nil" }
   ];
 
