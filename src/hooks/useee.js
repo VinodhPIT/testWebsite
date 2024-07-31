3da3430ecde6286100390232556c1bf26a55c809
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-08-01 01:44:51 +0530

    wdwdwd

--- src/hooks/useee.js
+++ src/hooks/useee.js
@@ -1,8 +1,7 @@
-
 import pathTranslations from '@/utils/pathTranslations';
 
-const   UsePathTranslation = (path, newLocale) => {
-  const locale = newLocale 
+const UsePathTranslation = (path, newLocale) => {
+  const locale = newLocale;
   const lng = locale.split('-')[1];
 
   // Split the path to identify base and dynamic segments
@@ -10,20 +9,26 @@ const   UsePathTranslation = (path, newLocale) => {
   const dynamicSegment = pathParts.pop(); 
   const basePath = pathParts.join('/'); 
 
+  // Construct dynamic base path with slug pattern
   const dynamicBasePath = `${basePath}/:slug*`;
+
+  // Check if there is a translation for dynamic base path
   if (pathTranslations[dynamicBasePath] && pathTranslations[dynamicBasePath][lng]) {
-    // Reconstruct the path with the translated base path and dynamic segment
     const translatedPath = pathTranslations[dynamicBasePath][lng].replace(':slug*', dynamicSegment);
     return translatedPath;
   }
+
+  // Check if there is a translation for base path without dynamic segments
   if (pathTranslations[basePath] && pathTranslations[basePath][lng]) {
     return pathTranslations[basePath][lng];
   }
 
+  // Check for exact match path translation
   if (pathTranslations[path] && pathTranslations[path][lng]) {
     return pathTranslations[path][lng];
   }
 
+  // Return the original path if no translation found
   return path;
 };
 
